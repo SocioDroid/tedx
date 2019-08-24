13c8ff6c7ad5adb9fef29d91356340678297a64e
sociodroid <sociodroid2019@gmail.com>
2019-08-24 14:53:31 +0530

    TEDxDYPCOE

---
+++ js/boxanimation.js
@@ -0,0 +1,36 @@
+$(".yDownAnimate").boxLoader({
+  direction: "y",
+  position: "-20%",
+  effect: "fadeIn",
+  duration: "1.5s",
+  windowarea: "50%"
+});
+
+$(".yUpAnimate").boxLoader({
+  direction: "y",
+  position: "100%",
+  effect: "fadeIn",
+  duration: "2s",
+  windowarea: "80%"
+});
+
+$(".xRightAnimate").boxLoader({
+  direction: "x",
+  position: "-50%",
+  effect: "fadeIn",
+  duration: "2s",
+  windowarea: "50%"
+});
+
+$(".xLeftAnimate").boxLoader({
+  direction: "x",
+  position: "50%",
+  effect: "fadeIn",
+  duration: "2s",
+  windowarea: "50%"
+});
+// Direction: Y
+// Position: 100%
+// Effect: None
+// Duration: 2s
+// Windowarea: 50%
